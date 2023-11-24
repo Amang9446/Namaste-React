2f0b5cca451da09d6d7bc536a5b9df87a346f76d
amang9446 <amang9446@gmail.com>
2023-11-24 13:30:38 +0530

    Indroduction to JSX

---
+++ app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = (<h1 id="jsx">I am JSX Heading</h1>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
--- index.js
+++
@@ -1,3 +0,0 @@
-const heading = React.createElement('h1', {id:'heading'},"I am H1", React.createElement('h2',{id:'heading2'}, 'I am H2'));
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(heading);
