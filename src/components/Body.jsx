619576849565be6f1b092d042f6e8d563c7c64d8
amang9446 <amang9446@gmail.com>
2023-11-28 17:21:21 +0530

    Hooks

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,33 @@
-import { resturantsList } from '../utils/static';
-import RestaurantCard from './RestaurantCard';
+import { useState } from "react";
+import { resturantsList } from "../utils/static";
+import RestaurantCard from "./RestaurantCard";
 
-const Body = () => (
+
+const Body = () => {
+  const [listofResturants, setlistofResturants] = useState(resturantsList)
+  return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listofResturants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setlistofResturants(filteredList)
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Resturants
+        </button>
+      </div>
       <div className="res-container">
-      {resturantsList.map((resData) => (
+        {resturantsList.map((resData) => (
           <RestaurantCard key={resData.data.id} resData={resData} />
         ))}
       </div>
     </div>
   );
+};
 
-  export default Body;
+export default Body;
