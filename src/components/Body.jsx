a0e3c72200d0765060c4de94af0c01e0e76dd452
amang9446 <amang9446@gmail.com>
2023-12-04 17:06:43 +0530

    useState() Hooks

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
-import { resturantsList } from "../utils/static";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 
-
 const Body = () => {
-  const [listofResturants, setlistofResturants] = useState(resturantsList)
+  const [listofResturants, setlistofResturants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const data = await fetch("https://corsproxy.io/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D28.7040592%26lng%3D77.10249019999999%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING");
+      const dataJson = await data.json();
+      const restaurants = dataJson.data.cards[5].card.card.gridElements?.infoWithStyle.restaurants;
+      console.log(restaurants);
+      setlistofResturants(restaurants);
+      setFilteredRestaurants(restaurants);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <div className="body">
       <div className="filter">
@@ -12,18 +29,18 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listofResturants.filter(
-              (res) => res.data.avgRating > 4
+              (res) => res.info.avgRating > 4
             );
-            setlistofResturants(filteredList)
+            setFilteredRestaurants(filteredList);
             console.log(filteredList);
           }}
         >
-          Top Rated Resturants
+          Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {resturantsList.map((resData) => (
-          <RestaurantCard key={resData.data.id} resData={resData} />
+        {filteredRestaurants?.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,15 +1,15 @@
 import { CDN_URL } from "../utils/constants";
 const RestaurantCard = (props) => {
   const { resData } = props;
-  const {name, cuisines, costForTwo, avgRating, deliveryTime} = resData?.data;
+  const {name, cuisines, costForTwo, avgRating, sla} = resData?.info;
   return (
     <div className="res-card">
-      <img className="res-logo" src={CDN_URL +resData.data.cloudinaryImageId} alt="res-img" />
+      <img className="res-logo" src={CDN_URL +resData.info.cloudinaryImageId} alt="res-img" />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4> 
-      <h4>₹{costForTwo/100} for two</h4>
+      <h4>{costForTwo}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>{deliveryTime} mins</h4>
+      <h4>{sla.deliveryTime} mins</h4>
     </div>
   );
 };
