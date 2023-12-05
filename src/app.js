0fff50693f563245c38d42ed41d483a826bd56b8
amang9446 <amang9446@gmail.com>
2023-12-05 16:32:41 +0530

    Routing and Error Handling

--- src/app.js
+++ src/app.js
@@ -2,11 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header"
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ContactUs from "./components/ContactUs";
+import AboutUs from "./components/AboutUs";
+import Error from "./components/Error";
+
 const AppLayout = () => (
   <div className="app">
     <Header />
     <Body />
   </div>
 );
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout/>,
+    errorElement: <Error/>
+  },
+  {
+    path: "/contact-us",
+    element: <ContactUs/>
+  },
+  {
+    path: "/about",
+    element: <AboutUs/>
+  }
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter}/>);
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,8 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>I am About Us Page</h1>
+    </div>
+  );
+};
+export default AboutUs;
