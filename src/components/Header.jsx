76416ff1624a67bc7c7d77f71d021eddfef118f7
amang9446 <amang9446@gmail.com>
2023-12-05 11:51:12 +0530

    Added Login Button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
-const Header = () => (
+const Header = () => {
+  const [btnNameReact, setbtnNameReact] = useState("login");
+  // const [userAdminBtn, setuserAdminBtn] = useState("I am user")
+  return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src= {LOGO_URL}
-          alt="logo"
-        />
+        <img className="logo" src={LOGO_URL} alt="logo" />
       </div>
       <div className="nav-items">
         <ul>
@@ -15,9 +15,29 @@ const Header = () => (
           <li>About Us</li>
           <li>Contact Us</li>
           <li>SignUp</li>
+          {/* <button className="user-btn"
+          onClick={()=>{
+            userAdminBtn === "I am user"
+            ? setuserAdminBtn("I am Admin")
+            : setuserAdminBtn("I am user")
+          }}
+          >
+            {userAdminBtn}
+          </button> */}
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnNameReact === "login"
+                ? setbtnNameReact("logout")
+                : setbtnNameReact("login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
   );
+};
 
-  export default Header;
+export default Header;
